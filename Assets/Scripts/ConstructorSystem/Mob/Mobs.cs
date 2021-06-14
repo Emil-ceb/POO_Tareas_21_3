74246c58fcf6148a7b65b7b698133b9f34010748
Emil-ceb <emiliocec@outlook.com>
2021-06-14 17:28:34 -0500

    Se modifico scripts de base y de enemigos

--- Assets/Scripts/ConstructorSystem/Mob/Mobs.cs
+++ Assets/Scripts/ConstructorSystem/Mob/Mobs.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Mobs : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int MobId;
+    public string names;
+    public float health;
+    public float speed;
+
+    baseMob bdMob = new baseMob();
     void Start()
     {
+        bdMob = GameObject.FindObjectOfType<baseMob>();
+        loadData(MobId);
         
+    }
+    void loadData(int id)
+    {
+        for (int i = 0; i < bdMob.newMob.Length; i++)
+        {
+            if (bdMob.newMob[i].MobId==id)
+            {
+                this.names=bdMob.newMob[i].name;
+                this.health=bdMob.newMob[i].health;
+                this.speed=bdMob.newMob[i].speed;
+            }
+            
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
